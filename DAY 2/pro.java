3b4ac4ef27b64080106e49334eea176c05de29e3
Riyancy <ithayathasriyancy68ir02@gmail.com>
2025-04-26 11:21:49 +0530

    Add files via upload

---
+++ DAY 2/pro.java
@@ -0,0 +1,13 @@
+//this is a comment
+//this is our first java program
+
+import java.io.*; //this is how we import packages 
+
+class FirstClass{ //class name should start with capital letter
+
+public static void main(String [] ar) {
+	System.out.println("The first argument is"+ar[0]);
+	System.out.println("The second argument is"+ar[1]);
+
+}
+}
