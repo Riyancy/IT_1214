bd4e55e96c7a94b1e05ea98004b731392ea321c9
Riyancy <ithayathasriyancy68ir02@gmail.com>
2025-04-26 11:19:05 +0530

    Add files via upload

---
+++ DAY 1/my.java
@@ -0,0 +1,13 @@
+//this is a comment
+//this is our first java program
+
+import java.io.*; //this is how we import packages 
+
+class myClass{ //class name should start with capital letter
+
+public static void main(String [] args) {
+	System.out.print("Hi");
+	System.out.print("This is IT1214 Practical Session 1");
+	System.out.println("This prints in a new line");
+}
+}
